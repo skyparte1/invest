43c6029dbf4e37910c5e831dbde8a925eb808667
Luis Fernando <lfmouts@gmail.com>
2026-08-19 23:17:10 -0300

    v0.1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+document.getElementById("current-year").textContent = new Date().getFullYear();
+
+document.querySelectorAll("#mainNav .nav-link, #mainNav .nav-actions a").forEach((link) => {
+    link.addEventListener("click", () => {
+        const navigation = document.getElementById("mainNav");
+        if (navigation.classList.contains("show")) {
+            bootstrap.Collapse.getOrCreateInstance(navigation).hide();
+        }
+    });
+});
