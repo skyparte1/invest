f82e5b05094be144f46c99f50ea7ba5e0cd3ad61
Luis Fernando <lfmouts@gmail.com>
2026-08-24 20:46:38 -0300

    v1.0.2 - Pequena correção de bugs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use App\Http\Middleware\SecurityHeaders;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,11 +16,19 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $trustedProxies = env('TRUSTED_PROXIES');
 
+        if ((! is_string($trustedProxies) || $trustedProxies === '') && env('APP_ENV') === 'production') {
+            $trustedProxies = '*';
+        }
+
         if (is_string($trustedProxies) && $trustedProxies !== '') {
             $middleware->trustProxies(
                 at: $trustedProxies === '*'
                     ? '*'
-                    : array_map('trim', explode(',', $trustedProxies))
+                    : array_map('trim', explode(',', $trustedProxies)),
+                headers: Request::HEADER_X_FORWARDED_FOR
+                    | Request::HEADER_X_FORWARDED_HOST
+                    | Request::HEADER_X_FORWARDED_PORT
+                    | Request::HEADER_X_FORWARDED_PROTO
             );
         }
 
